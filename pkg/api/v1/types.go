1b71aa087228280a8ccfd4de1de12d0ad02dd771
Pablo Mercado <odacremolbap@gmail.com>
2017-06-17 12:59:58 +0200

    add fstype and RO to DO volumes type

--- pkg/api/v1/types.go
+++ pkg/api/v1/types.go
@@ -1301,6 +1301,14 @@ type StorageOSPersistentVolumeSource struct {
 type DOVolumeSource struct {
 	// Unique ID for the volume resource at Digital Ocean.
 	VolumeID string `json:"volumeID" protobuf:"bytes,1,opt,name=volumeID"`
+	// FSType represents the filesystem type to mount
+	// Must be a filesystem type supported by the host operating system.
+	// Ex. "ext4", "xfs". Implicitly inferred to be "ext4" if unspecified.
+	FSType string `json:"fsType,omitempty" protobuf:"bytes,2,opt,name=fsType"`
+	// Defaults to false (read/write). ReadOnly here will force
+	// the ReadOnly setting in VolumeMounts.
+	// +optional
+	ReadOnly bool `json:"readOnly,omitempty" protobuf:"varint,3,opt,name=readOnly"`
 }
 
 // Adapts a ConfigMap into a volume.
